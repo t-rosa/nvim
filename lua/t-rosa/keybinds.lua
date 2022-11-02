874f8fb9de2a649b7edbebe245664853f9504c23
Toma Rosa <toma.rosa@protonmail.com>
2022-11-02 09:55:33 +0100

    Keybind edit

--- lua/t-rosa/keybinds.lua
+++ lua/t-rosa/keybinds.lua
@@ -15,11 +15,11 @@ vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<cr>', op
 vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
 vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
 vim.keymap.set('n', '<leader>dl', 'v<cmd>lua vim.diagnostic.setloclist()<cr>', opts)
-vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-vim.keymap.set('n', 'gh', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-vim.keymap.set('n', 'ga', "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+vim.keymap.set('n', '<leader>gd', "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+vim.keymap.set('n', '<leader>gh', "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+vim.keymap.set('n', '<leader>ga', "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 vim.keymap.set('n', '<leader>r', "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-vim.keymap.set('n', '<leader>f', "<cmd>lua vim.lsp.buf.format()<cr>", opts)
+vim.keymap.set('n', '<leader>p', "<cmd>lua vim.lsp.buf.format()<cr>", opts)
 
 -- Telescope
 vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<cr>", opts)
@@ -27,13 +27,12 @@ vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", opts)
 vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", opts)
 
 -- Explorer
-vim.keymap.set('n', '<leader>ee', "<cmd>NvimTreeToggle<cr>", opts)
-vim.keymap.set('n', '<leader>ec', "<cmd>NvimTreeCollapse<cr>", opts)
+vim.keymap.set('n', '<leader>e', "<cmd>NvimTreeToggle<cr>", opts)
+vim.keymap.set('n', '<leader>c', "<cmd>NvimTreeCollapse<cr>", opts)
 
 -- Hop
 vim.keymap.set('n', '<leader>hw', "<cmd>HopWord<cr>", opts)
 vim.keymap.set('n', '<leader>hc', "<cmd>HopChar1<cr>", opts)
-vim.keymap.set('n', '<leader>hcc', "<cmd>HopChar2<cr>", opts)
 
 
 -- Text move up and down

bcf9dc90c0269150990893c8428c21200cdd23f1
Toma <toma.rosa@protonmail.com>
2022-11-11 11:29:26 +0100

    add few more options

--- lua/t-rosa/options.lua
+++ lua/t-rosa/options.lua
@@ -3,10 +3,15 @@ vim.opt.relativenumber = true
 
 vim.opt.smartindent = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop=4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
-vim.scrolloff = 8
+vim.opt.scrolloff = 8
 vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+
+vim.opt.swapfile = false
 
 vim.g.mapleader = " "
